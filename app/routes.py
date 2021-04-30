96d9e37d6eed283324ad4d2526f216ed850a092a
Sahari <savilescante@gmail.com>
2021-04-30 12:22:46 -0500

    created endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,40 @@
-from flask import Blueprint
-
-hello_world_bp = Blueprint("hello_world", __name__)
-
-
-# @hello_world_bp.route("/hello-world", methods=["GET"])
-# def say_hello_world():
-#     my_beautiful_response_body = "Hello, World!"
-#     return my_beautiful_response_body
 
 
 from app import db
-from app.models.book import Book
+from app.models.planet import Planet
 from flask import request, Blueprint, make_response, jsonify
 
-books_bp = Blueprint("books", __name__, url_prefix="/books")
+planet_bp = Blueprint("planet", __name__, url_prefix="/planets")
 
 
-@books_bp.route("", methods=["GET","POST"])
-def handle_books():
+@planet_bp.route("", methods=["GET","POST"])
+def handle_planets():
     if request.method == "GET":
-        books = Book.query.all()
-        books_response = []
-        for book in books:
-            books_response.append({
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description
             })
-        return jsonify(books_response)
+        return jsonify(planets_response)
     elif request.method == "POST":
         request_body = request.get_json()
-        new_book = Book(title=request_body["title"],
+        new_planet = Planet(title=request_body["name"],
                         description=request_body["description"])
 
-        db.session.add(new_book)
+        db.session.add(new_planet)
         db.session.commit()
 
-        return make_response(f"Book {new_book.title} successfully created", 201)
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book = Book.query.get(book_id)
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = Planet.query.get(planet_id)
 
     return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
+        "id":planet.id,
+        "name": planet.name,
+        "description": planet.description
     }
